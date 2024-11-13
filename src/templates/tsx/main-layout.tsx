f515b284ea67dd56da25e43e721ea3b91645b387
konyogony <45127834+konyogony@users.noreply.github.com>
2024-11-13 14:56:02 +0700

    Make icons integrate better into phragraphs(should add for headings aswell)

--- src/templates/tsx/main-layout.tsx
+++ src/templates/tsx/main-layout.tsx
@@ -34,7 +34,7 @@ const RootLayout = async ({
                 <main>
                     <div className='relative flex flex-row justify-center space-x-8 py-24'>
                         <Sidebar structure={structure} />
-                        <div className='prose prose-$COLOR prose-invert prose-p:my-4 prose-headings:my-2 prose-headings:mt-4 prose-headings:w-full prose-headings:border-b prose-headings:border-white/15 prose-ul:mb-4 prose-ol:mb-4 prose-headings:pb-1.5 prose-a:decoration-dotted hover:prose-a:text-$ACCENT-COLOR-500 prose-ol:my-0 prose-ul:my-0 prose-hr:border-white/20 flex h-fit w-screen flex-shrink-0 flex-col items-start lg:max-w-[40%] lg:px-0'>
+                        <div className='prose prose-$COLOR prose-invert prose-p:flex prose-p:items-center prose-p:gap-4 prose-p:my-4 prose-headings:my-2 prose-headings:mt-4 prose-headings:w-full prose-headings:border-b prose-headings:border-white/15 prose-ul:mb-4 prose-ol:mb-4 prose-headings:pb-1.5 prose-a:decoration-dotted hover:prose-a:text-$ACCENT-COLOR-500 prose-ol:my-0 prose-ul:my-0 prose-hr:border-white/20 flex h-fit w-screen flex-shrink-0 flex-col items-start lg:max-w-[40%] lg:px-0'>
                             <DocsBreadcrumbs />
                             {children}
                             <div className='mb-4 mt-8 h-[1px] w-full bg-white/15' />
