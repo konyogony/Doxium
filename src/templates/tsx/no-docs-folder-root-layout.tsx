64510038fc6e1e3680be7126df7f3002ff7e6ebf
konyogony <me@konyogony.dev>
2024-12-26 23:21:25 +0700

    Make marker lighter

--- src/templates/tsx/no-docs-folder-root-layout.tsx
+++ src/templates/tsx/no-docs-folder-root-layout.tsx
@@ -35,7 +35,7 @@ const RootLayout = async ({
                     <Navbar structure={structure} />
                     <div className='relative flex flex-row min-h-screen justify-center gap-8 pt-16 pb-2 lg:pt-24'>
                         <Sidebar structure={structure} separate={separate} />
-                        <div className='prose prose-base prose-invert flex h-fit w-screen flex-shrink-0 flex-col items-start px-6 prose-headings:my-2 prose-headings:w-full prose-headings:border-white/15 prose-h1:my-4 prose-h1:mt-4 prose-h1:border-b prose-h1:pb-2 prose-p:my-2 prose-a:decoration-dotted hover:prose-a:text-accent-600 prose-ol:my-0 prose-ol:mb-4 prose-ul:my-0 prose-ul:mb-4 prose-li:my-0.5 prose-hr:border-white/20 lg:max-w-[40%] lg:px-0'>
+                        <div className='prose prose-base prose-invert flex h-fit w-screen flex-shrink-0 flex-col items-start px-6 marker:text-base-200 prose-headings:my-2 prose-headings:w-full prose-headings:border-white/15 prose-h1:my-4 prose-h1:mt-4 prose-h1:border-b prose-h1:pb-2 prose-p:my-2 prose-a:decoration-dotted hover:prose-a:text-accent-600 prose-ol:my-0 prose-ol:mb-4 prose-ul:my-0 prose-ul:mb-4 prose-ul:list-inside prose-li:my-0.5 prose-hr:border-white/20 lg:max-w-[40%] lg:px-0'>
                             <Breadcrumbs />
                             {children}
                             <div className='mb-4 mt-8 h-[1px] w-full bg-white/15' />
